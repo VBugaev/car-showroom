24d32ccb9a8726c8135c153579909c0a404cf571
Vladimir Bugaev <vladimirBugaevv@gmail.com>
2018-12-18 02:06:51 +0400

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ app.prepare()
 .then(() => {
   const server = express()
 
-  server.get('/p/:id', (req, res) => {
-    const actualPage = '/post'
-    const queryParams = { title: req.params.id } 
-    app.render(req, res, actualPage, queryParams)
-  })
   server.get('/', (req, res) => {
     app.render(req, res, '/login');
   })
